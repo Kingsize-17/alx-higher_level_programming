6fb4ee244adaae619b995ded46da9fdc4065f802
Kingsize-17 <okokokingsize1@gmail.com>
2023-08-08 16:09:38 -0700

    quick update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-# 6-print_comb3.py
-
-"""Print all possible different combinations of two digits in an ascending order."""
-
 for i in range(9):
     for j in range(i + 1, 10):
         if i * 10 + j < 89:
